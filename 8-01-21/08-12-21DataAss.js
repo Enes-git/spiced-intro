01a6da1fa0d72d375692aeea5c2b47bbbadbe5c5
enes <enes.3.ergun@gmail.com>
2021-01-10 05:31:14 +0100

    folder name change

---
+++ 8-01-21/08-12-21DataAss.js
@@ -0,0 +1,50 @@
+// 1:
+function logType(arg) {
+    if (typeof arg == "number") {
+        if (isNaN(arg)) {
+            console.log("not a number!");
+        } else {
+            console.log("number!");
+        }
+    } else if (typeof arg == "object") {
+        if (arg === null) {
+            console.log("null!");
+        } else if (Array.isArray(arg)) {
+            console.log("array!");
+        } else {
+            console.log("object!");
+        }
+    } else if (typeof arg == "function") {
+        console.log("function!");
+    } else if (typeof arg == "string") {
+        console.log("string!");
+    } else if (typeof arg == "boolean") {
+        console.log("boolean!");
+    } else if (typeof arg == "bigint") {
+        console.log("bigint!");
+    } else if (typeof arg == "undefined") {
+        console.log("undefined!");
+    } else {
+        console.log("I have no idea!");
+    }
+}
+
+// 2:
+var a = {
+    Berlin: "Germany",
+    Paris: "France",
+    "New York": "USA",
+};
+
+var b = {};
+
+for (var i in a) {
+    b[a[i]] = i;
+}
+
+console.log(b);
+
+// 3:
+for (var i = 10; i >= 1; i--) {
+    console.log(i);
+}
