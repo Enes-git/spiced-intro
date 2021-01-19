a77ccd416a324e5c8a447892d01e73603fd67466
enes <enes.3.ergun@gmail.com>
2021-01-19 17:58:47 +0100

    kitty

--- 3rd Week/19-1-21/Kitty Carousel/script.js
+++ 3rd Week/19-1-21/Kitty Carousel/script.js
@@ -1,12 +1,19 @@
-var images = document.querySelectorAll(".kitty-container");
+var images = document.querySelectorAll(".kitty-container img");
+// console.log(images);
 var visibleKitty = 0;
 function moveKitties() {
     images[visibleKitty].classList.remove("onscreen");
     images[visibleKitty].classList.add("exit");
     visibleKitty++;
+    if (visibleKitty > 3) {
+        visibleKitty = 0;
+        return moveKitties();
+    }
+
     images[visibleKitty].classList.add("onscreen");
-    setTimeout(moveKitties, 5500);
+    setTimeout(moveKitties, 3500)
 }
+
 setTimeout(moveKitties, 1500);
 
 images[visibleKitty].addEventListener("trasitionend", moveKitties);
