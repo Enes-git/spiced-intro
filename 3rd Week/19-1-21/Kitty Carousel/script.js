d637f869c58ab7c6687fefa7789a382ee4f14347
enes <enes.3.ergun@gmail.com>
2021-01-19 21:02:24 +0100

    spinning kitties

--- 3rd Week/19-1-21/Kitty Carousel/script.js
+++ 3rd Week/19-1-21/Kitty Carousel/script.js
@@ -1,19 +1,23 @@
-var images = document.querySelectorAll(".kitty-container img");
-// console.log(images);
-var visibleKitty = 0;
-function moveKitties() {
-    images[visibleKitty].classList.remove("onscreen");
-    images[visibleKitty].classList.add("exit");
-    visibleKitty++;
-    if (visibleKitty > 3) {
-        visibleKitty = 0;
-        return moveKitties();
+(function () {
+    var images = document.querySelectorAll(".kitty-container img");
+    var visibleKitty = 0;
+
+    function moveKitties() {
+        images[visibleKitty].classList.remove("onscreen");
+        images[visibleKitty].classList.add("exit");
+
+        visibleKitty++;
+        if (visibleKitty == images.length) {
+            visibleKitty = 0;
+        }
+        images[visibleKitty].classList.add("onscreen");
+        setTimeout(moveKitties, 3500);
     }
+    setTimeout(moveKitties, 1500);
 
-    images[visibleKitty].classList.add("onscreen");
-    setTimeout(moveKitties, 3500)
-}
-
-setTimeout(moveKitties, 1500);
-
-images[visibleKitty].addEventListener("trasitionend", moveKitties);
+    document.addEventListener("transitionend", function (event) {
+        if (event.target.classList == "exit") {
+            event.target.classList.remove("exit");
+        }
+    });
+})();
