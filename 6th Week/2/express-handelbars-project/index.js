6c054f1ea2c6bff1522d3e2fcbf44e13b8a03215
enes <enes.3.ergun@gmail.com>
2021-02-10 05:26:15 +0100

    some minor css added

--- 6th Week/2/express-handelbars-project/index.js
+++ 6th Week/2/express-handelbars-project/index.js
@@ -18,45 +18,8 @@ app.get('/', (request, response) => {
 app.get('/about', (request, response) => {
     response.render('about', {
         layout: 'main',
-        emojis: ['whale', 'vampire', 'poop'],
+        aboutMe: `I am ...`,
     });
 });
 
 app.listen(8080, () => console.log("I'm all ears!"));
-
-// =========================================
-// ==========================================================
-// const cookieParser = require('cookie-parser');
-
-// const express = require('express');
-// var app = express();
-
-// // cookie middleware
-// app.use(cookieParser());
-
-// // serving the files from project folder
-// app.use(express.static(__dirname + '/projects'));
-// // a middlewware to check if cookies accepted
-// // and and if statement
-
-// app.get('/cookie', (request, response) => {
-//     response.send(`
-//         <h3>Cookies Warning</h3>
-//         <p>You must accept our cookie policy in order to continue using our site.</p>
-//         <form method="POST">
-//         <input type="checkbox" name="cookie-on"><span>Do you want some cookie?</span></input>
-//         <button> Submit </button>
-//         </form>`);
-// });
-
-// app.post('/cookie', (request, response) => {
-//     const { accept } = request.body;
-//     if (accept) {
-//         response.cookie('authentication', 'true');
-//     }
-// });
-
-// // checking the server
-// app.listen(8080, () => {
-//     console.log('I am all ears!');
-// });
